de5a344988ddef8680c9d280a99b25b49767ba55
shawn <shawn.trent@live.com>
2020-06-15 19:44:33 -0600

    123

--- ex16.py
+++ ex16.py
@@ -22,12 +22,12 @@ line = input("line 3: ")
 
 print("I'm going to write these to the file.")
 
-target.write('line1')
-target.write("\n)")
-target.write('line2')
-target.write("\n)")
-target.write('line3')
-target.write("\n)")
+target.write("line1")
+target.write("\n")
+target.write("line2")
+target.write("\n")
+target.write("line3")
+target.write("\n")
 
 print("And finally, we close it.")
 target.close()
